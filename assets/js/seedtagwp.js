6a2f250ae2071b45584bdf0588a7ba2347bf1d5e
seedtag <seedtag@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-20 15:28:16 +0000

    some readme fixes
    git-svn-id: https://plugins.svn.wordpress.org/seedtag/tags/0.1.1@1010713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/seedtagwp.js
@@ -0,0 +1,8 @@
+/*! seedtagwp - v0.1.0
+ * hrrp://seedtag.com
+ * Copyright (c) 2014; * Licensed GPLv2+ */
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
---
+++ assets/js/src/seedtagwp.js
@@ -0,0 +1,13 @@
+/**
+ * seedtagwp
+ * hrrp://seedtag.com
+ *
+ * Copyright (c) 2014 Jesus Cruz Hermoso
+ * Licensed under the GPLv2+ license.
+ */
+ 
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
